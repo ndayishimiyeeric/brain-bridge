986a946abb9e48e0b64615d94a81a77919026363
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-09-29 20:57:37 +0200

    add course page

---
+++ src/app/(dashboard)/(routes)/teacher/courses/(routes)/[courseId]/page.tsx
@@ -0,0 +1,69 @@
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+import FormTitle from "./_components/title-form";
+
+type Props = {
+  params: {
+    courseId: string;
+  };
+};
+
+const CoursePage = async ({ params }: Props) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      userId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.price,
+    course.imageUrl,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completedText = `(${completedFields}/${totalFields})`;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Completed fields{completedText}
+          </span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <FormTitle initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoursePage;
