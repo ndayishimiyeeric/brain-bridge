76c791bce302a33715dea2ffaf95d1fe4a0feac5
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-03 06:48:34 +0200

    add marking course as complete

---
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } },
+) {
+  try {
+    const { userId } = auth();
+    const { courseId, chapterId } = params;
+    const { isCompleted } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userProgress = await db.userProgress.upsert({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId,
+        },
+      },
+      update: {
+        isCompleted,
+      },
+      create: {
+        userId,
+        chapterId,
+        isCompleted,
+      },
+    });
+
+    return NextResponse.json(userProgress);
+  } catch (e) {
+    console.error(e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
